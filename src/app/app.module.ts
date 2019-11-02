a3bff5eba645ac2239a836a09bd2b288e3260518
Nilo Almeida <nilopaivadealmeida@gmail.com>
2019-11-02 20:15:55 -0300

    criando componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { AddTaskComponent } from './components/dashboard/add-task/add-task.component';
+import { ListTaskComponent } from './components/dashboard/list-task/list-task.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    AddTaskComponent,
+    ListTaskComponent
   ],
   imports: [
     BrowserModule,
